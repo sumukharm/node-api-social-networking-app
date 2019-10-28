3e768c56dba34aa31e9a6ed01b90f5d48fa45f8b
sumukharm <sumukha.mohan@digisignals.com>
2019-10-28 15:23:34 -0700

    port 3001 for node api instead of 8080

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(function(err, req, res, next) {
     }
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`A Node Js API is listening on port: ${port}`);
 });
